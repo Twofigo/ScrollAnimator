ccf4eeaa7287f6db98eae970d32d113a3f3b47fc
August Baaz <august.baaz@gmail.com>
2019-03-09 18:26:48 +0100

    created a demo, made if work, and generalized stuff
    basically shit ton of code but no commits

--- source/JS.js
+++ source/JS.js
@@ -1,25 +1,44 @@
 function setup(){
+    var animator = scrollAnimator.newAnimator(document.getElementById("box"));
+    var pLeft = animator.addAnimation("left");
+    pLeft.addKeyframe(100,300);
+    pLeft.addKeyframe(400,1500);
+    pLeft.addKeyframe(10,6000);
     
-    
+    scrollAnimator.init();
 }
 
 var scrollAnimator = (function(){
-    var objects = [];
+
+    var animators = [];
+    
+    var syntaxFunctions = {};
+    syntaxFunctions.distance = function(dom, attribute, value){
+        dom.style[attribute]= value+"px";
+    }
+    
+    var transissions = {};
+    transissions.linear = function(timeStamp){
+        return timeStamp;
+    }
+    transissions.constant = function(timeStamp){
+        return 0;
+    }
     
     var init = function(){
-        var self = this;
-        document.body.addEventListener("scroll", self.update);
+       window.addEventListener('scroll', update)
+ 
     }
     
     var update = function(){
-        for (obj of objects){
-            obj.update(document.scrollTop)
+        for (a of animators){
+            a.update(document.body.scrollTop);
         }
     }
     
     var newAnimator = function(dom){
         var animator = new Animator(dom);
-        objects.push(animator)
+        animators.push(animator)
         return animator;
     }
 
@@ -34,69 +53,100 @@ var scrollAnimator = (function(){
        }
        return t;
     }
-    Animator.prototype.add = function(attribute){
-        this.animations.push(attribute);
+    Animator.prototype.addAnimation = function(attribute, syntaxMode=false){     
+        var animation = new Animation(attribute, syntaxMode);
+        this.animations.push(animation);
+        return animation;
     }
-    Animator.prototype.add = function(cssAttribute, valueObj, unit=false, inline=false){
-        if (unit == false){ // switch statement for autodetect
-            
-        }            
-        this.add(new Animation(cssAttribute, valueObj, unit));
-    }
-    Animator.prototype.update = function(TimeStamp){
+    Animator.prototype.update = function(timeStamp){
         for (a of this.animations){
             a.update(this.dom, timeStamp)
         }
     }
     
-    var Animation = function(cssAttribute, valueObj, unit, transissionFunc){
-        this.cssAttribute = cssAttribute;
-        this.valueObj = valueObj;
-        this.unit = unit || false;
+    var Animation = function(attribute, syntaxMode=false){
+        this.attribute = attribute;
         this.keyframes = [];
+        
+        if (syntaxMode.typeof)this.syntaxFunction;
+        
+        
+        this.addKeyframe(0,0);
+        
+        
+        this._keyframe;
+        this._valueDiff;
+        this._timeDiff;
     }
     Animation.prototype.clone = function(){
-       var t = new Animation(this.cssAttribute, this.valueObj, this.unit, this.transissionFunc, this.inline);
+       var t = new Animation(this.cssAttribute);
+       t.syntaxFunction = this.syntaxFunction;
+       for(k in this.keyframe){
+            t.addKeyframe(k.clone());
+       }
        return t;
     }
-    Animation.prototype.addKeyframe(keyframe){
+    Animation.prototype.addKeyframe = function(value, timeStamp, transission = transissions.linear){
+        var keyframe = new Keyframe(value, timeStamp, transission)
         this.keyframes.push(keyframe);
         this.keyframes.sort(function(a,b){return a.timeStamp - b.timeStamp});
-    }
-    Animation.prototype.addKeyframe(value, timeStamp, transissionFunc){
-        this.addKeyframe(new Keyframe(value, timeStamp, transissionFunc));
+        return keyframe;
     }
     Animation.prototype.update = function(dom, timeStamp){
-        var last_keyframe = false;
-        var next_keyframe = false;
-        for(k in this.keyframe){
-            if (k.timeStamp > timeStamp){
-                if (!last_keyframe){
-                    last_keyframe = k;
-                    continue;
+        
+        if(!this._keyframe || this._keyframe.timeStamp>timeStamp || (this._keyframe.timeStamp+this._timeDiff)<timeStamp){
+            var last_keyframe = false;
+            var next_keyframe = false;
+            for(k of this.keyframes){
+                if (k.timeStamp > timeStamp){
+                    next_keyframe = k;
+                    break;
                 }
-                next_keyframe = k;
-                break;
+                last_keyframe = k;
+            }
+            this._keyframe = last_keyframe;
+            if  (next_keyframe){
+                this._timeDiff = next_keyframe.timeStamp - last_keyframe.timeStamp;
+                
+                this._valueDiff = false;
+                for(key in this._keyframe.value){
+                     this._valueDiff[key] = next_keyframe.value[key] - last_keyframe.value[key];
+                }
+                if (!this._valueDiff){
+                     this._valueDiff = next_keyframe.value - last_keyframe.value;
+                }
+            }
+            else{
+                this._valueDiff = false;
+                this._timeDiff = false;
             }
         }
         
-        var valueDiff = next_keyframe.value - last_keyframe.value;
-        var timeDiff = next_keyframe.timeStamp - last_keyframe.timeStamp;
-        var string;
-        string = (timeStamp-last_keyframe.timeStamp)/timeDiff;
-        if (last_keyframe.transissionFunc){
-            string = last_keyframe.transissionFunc(string)
+        var v;
+            
+        if (this._valueDiff){
+            var factor = this._keyframe.transission((timeStamp-this._keyframe.timeStamp)/this._timeDiff);
+            
+            for(key in this._keyframe.value){
+                 v[key] = this._valueDiff[key]*factor;
+                 v[key] += this._keyframe.value[key];
+            }
+            if (!v){
+                 v = this._valueDiff*factor;
+                 v += this._keyframe.value;
+            }
         }
-        string *= valueDiff;
-        string += last_keyframe.value;
-        string += unit;
-        dom.style['this.cssAttribute']= string;
+        else{
+            v = this._keyframe.value;
+        }    
+        
+        syntaxFunctions.distance(dom, this.attribute, v);
     }
     
-    var Keyframe = function(value, timeStamp, transissionFunc){
+    var Keyframe = function(value, timeStamp, transission = transissions.linear){
         this.value = value;
         this.timeStamp = timeStamp;
-        this.transissionFunc = transissionFunc || false;
+        this.transission = transission;
     }
     Keyframe.prototype.clone = function(){
        var t = new Keyframe(value, timeStamp, this.transissionFunc);
@@ -105,6 +155,7 @@ var scrollAnimator = (function(){
     
     return {
         init: init,
+        animators: animators,
         newAnimator: newAnimator,
     };
 })();
